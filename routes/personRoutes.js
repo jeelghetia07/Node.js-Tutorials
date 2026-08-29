26d973a6b96a40813336d4fe6a3d6d5b7868335b
jeelghetia07 <jeelghetia07@gmail.com>
2026-08-29 19:49:31 +0530

    for ease of endpoints

---
+++ routes/personRoutes.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const Person = require('../models/person');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try{
+        const data = req.body;
+
+        // create a new entry for DB.
+        const newPerson = new Person(data);
+
+        // save into the DB.
+        const resp = await newPerson.save();
+        console.log('Person saved');
+        res.status(200).json(resp);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: "internal error occured"});
+    }
+});
+
+router.get('/', async (req, res) => {
+    try{
+        const data = await Person.find();
+        console.log('Items found');
+        res.status(200).json(data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: "internal error occured"});
+    }
+})
+
+router.get('/:workType', async (req, res) => {
+    try {
+        const workType = req.params.workType;
+        if(workType == 'chef' || workType == 'manager' || workType == 'waiter'){
+            const resp = await Person.find({work: workType});
+            console.log('response fetched');
+            res.status(200).json(resp);
+        }else{
+            res.status(404).json({error : "Invalid workTYpe"});
+        }
+    } catch (error) {
+        console.log(err);
+        res.status(500).json({err: "Internal Server Error"});
+    }
+})
+
+module.exports = router
