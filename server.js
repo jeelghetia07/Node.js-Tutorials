512d1481d83c52afc0b473ff0992b810abb5acaf
jeelghetia07 <jeelghetia07@gmail.com>
2026-08-29 14:00:20 +0530

    made some REST API, and tested them

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 const express = require('express')
 const app = express()
+const db = require('./db');
+const { model } = require('mongoose');
+
+const Person = require('./models/person');
 
 app.get('/', function(req, res){
     res.send('Hello world');
 })
 
-app.get('/rice', (req, res) => {
-    res.send('Sure, let me bring fried rice now....');
-})
+// app.get('/rice', (req, res) => {
+//     res.send('Sure, let me bring fried rice now....');
+// })
 
-app.get('/idli', (req, res) => {
-    var custom_idli = {
-        name: 'rava idli',
-        size: '10 cm daimeter',
-        is_sambhar: true,
-        is_chutney: false
+// app.get('/idli', (req, res) => {
+//     var custom_idli = {
+//         name: 'rava idli',
+//         size: '10 cm daimeter',
+//         is_sambhar: true,
+//         is_chutney: false
+//     }
+//     res.send(custom_idli);
+// })
+
+// app.post('/items', (req, res) => {
+//     res.send("Data is saved");
+// })
+
+
+app.post('/person', async (res, req) => {
+    try{
+        const data = req.body;
+        const newPerson = new Person(data);
+
+        const savedPerson = await newPerson.save();
+    }catch(err){
+        
     }
-    res.send(custom_idli);
-})
-
-app.post('/items', (req, res) => {
-    res.send("Data is saved");
 })
 
 app.listen(3000, () => {
