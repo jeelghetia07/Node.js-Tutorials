1ae2fa430326691e1cd218221470649422ef03b5
jeelghetia07 <jeelghetia07@gmail.com>
2026-08-29 13:55:26 +0530

    Made connected to DB

---
+++ db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// define the mongoDB url
+const mongoURL = 'mongodb://localhost:27017/hotels';
+
+// setup mongoDB connection.
+mongoose.connect(mongoURL);
+
+// get the default connection.
+// mongoose maintains a default connection object represnting the mongoDB connection.
+const db = mongoose.connection;
+
+// define event litseners for database connection.
+db.on('connected', () => {
+    console.log("connected to mongoDB server.");
+});
+
+db.on('disconnected', () => {
+    console.log("mongoDB connection error.", err);
+});
+
+db.on('error', () => {
+    console.log("mongoDB disconnected");
+});
+
+// exports the database connection to the server file.
+module.exports = db;
