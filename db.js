808000802e729668ce1213af191382a5835a618c
jeelghetia07 <jeelghetia07@gmail.com>
2026-09-05 13:40:34 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // define the mongoDB url
-// const mongoURL = 'mongodb://localhost:27017/hotels';
-const mongoURL = process.env.DB_URL;
+const mongoURL = 'mongodb://localhost:27017/hotels';
+// const mongoURL = process.env.DB_URL;
 // setup mongoDB connection.
 mongoose.connect(mongoURL);
 
